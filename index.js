ce2b9f1784b28a0515d18dae9d9f194f2a6f8a01
Phu <phugnguyen@gmail.com>
2019-05-16 15:36:33 -0700

    request data using fetch

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const url = "https://api-pub.bitfinex.com/v2";
-// Available values: '5m', '15m','1h', '3h', '6h', '12h', '1D', '1M'
+
+// Available values: '5m', '15m','1h', '3h',
+//                   '6h', '12h', '1D', '1M'
+
+// query parmas limit(number of candles, max:5000)
+// start(ms) end(ms) sort(if = 1 it sorts old to new)
+
 let timeFrame = "15m";
 let ticker = "tBTCUSD";
 
 const proxyUrl = "https://cors-anywhere.herokuapp.com/",
-  targetUrl = `${url}/candles/trade:${timeFrame}:${ticker}/hist`;
+  targetUrl = `${url}/candles/trade:${timeFrame}:${ticker}/hist?limit=365`;
 fetch(proxyUrl + targetUrl)
-  .then(blob => blob.json())
+  .then(response => response.json())
   .then(data => {
     console.table(data);
     return data;
