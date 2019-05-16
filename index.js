d277070f2e9c8694f12185db4e13c0789f9a4e6d
Phu <phugnguyen@gmail.com>
2019-05-16 15:42:16 -0700

    add comments on data structure and type

--- index.js
+++ index.js
@@ -21,3 +21,12 @@ fetch(proxyUrl + targetUrl)
     console.log(e);
     return e;
   });
+
+//Response Details
+// Fields	Type	           Description
+// MTS	  int	             millisecond time stamp
+// OPEN	  float	           First execution during the time frame
+// CLOSE	float	           Last execution during the time frame
+// HIGH	  float	           Highest execution during the time frame
+// LOW	  float	           Lowest execution during the timeframe
+// VOLUME	float	           Quantity of symbol traded within the timeframe
